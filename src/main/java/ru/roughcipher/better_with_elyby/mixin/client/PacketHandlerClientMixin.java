4bcbc34f993d01431501c7f29ca602c3548ae4a6
RoughCipher <roughcipher@gmail.com>
2026-06-10 19:00:00 +0300

    Исправление авторизации.

--- src/main/java/ru/roughcipher/better_with_elyby/mixin/client/PacketHandlerClientMixin.java
+++ src/main/java/ru/roughcipher/better_with_elyby/mixin/client/PacketHandlerClientMixin.java
@@ -1,18 +1,23 @@
 package ru.roughcipher.better_with_elyby.mixin.client;
 
+import java.net.URL;
 import net.minecraft.client.net.handler.PacketHandlerClient;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(value = PacketHandlerClient.class, remap = false)
 public class PacketHandlerClientMixin {
 
-    @ModifyConstant(
-        method = "handleHandshake",
-        constant = @Constant(stringValue = "http://session.minecraft.net/game/joinserver.jsp?user=")
-    )
-    private String ElyBySessionUrl(String orig) {
-        return "https://authserver.ely.by/session/legacy/join?user=";
-    }
+	@Redirect(
+		method = "handleHandshake",
+		at = @At(value = "NEW", target = "java/net/URL", args = "Ljava/lang/String;")
+	)
+	private URL redirectSessionUrl(String urlString) throws Exception {
+		String newUrl = urlString.replace(
+			"http://session.minecraft.net/game/joinserver.jsp?user=",
+			"https://authserver.ely.by/session/legacy/join?user="
+		);
+		return new URL(newUrl);
+	}
 }
--- src/main/java/ru/roughcipher/better_with_elyby/mixin/server/PacketHandlerLoginMixin.java
+++ src/main/java/ru/roughcipher/better_with_elyby/mixin/server/PacketHandlerLoginMixin.java
@@ -1,18 +1,81 @@
 package ru.roughcipher.better_with_elyby.mixin.server;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLEncoder;
+import net.minecraft.core.net.packet.PacketLogin;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.net.handler.PacketHandlerLogin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(value = PacketHandlerLogin.class, remap = false)
-public class PacketHandlerLoginMixin {
+public abstract class PacketHandlerLoginMixin {
 
-    @ModifyConstant(
-        method = "handleLogin",
-        constant = @Constant(stringValue = "http://session.minecraft.net/game/checkserver.jsp?user=")
-    )
-    private String ElyByCheckServerUrl(String orig) {
-        return "https://authserver.ely.by/session/legacy/hasJoined?user=";
-    }
+	@Shadow
+	private static Logger LOGGER;
+	@Shadow
+	private String username;
+	@Shadow
+	private MinecraftServer mcServer;
+	@Shadow
+	private String serverId;
+	@Shadow
+	public abstract void kickUser(String message);
+	@Shadow
+	public abstract void doLogin(PacketLogin packetLogin);
+	@Shadow
+	public abstract String getUserAndIPString();
+	@Shadow
+	public static String getServerId(PacketHandlerLogin handler) {
+		return null;
+	}
+	@Shadow
+	public static PacketLogin setLoginPacket(PacketHandlerLogin handler, PacketLogin packet) {
+		return null;
+	}
+
+	@Overwrite
+	public void handleLogin(PacketLogin packetLogin) {
+		this.username = packetLogin.username;
+
+		if (packetLogin.playerEntityIdAndProtocolVersion != 32786) {
+			if (packetLogin.playerEntityIdAndProtocolVersion > 32786) {
+				this.kickUser("Outdated server!");
+			} else {
+				this.kickUser("Outdated client!");
+			}
+			return;
+		}
+
+		if (!this.mcServer.onlineMode) {
+			this.doLogin(packetLogin);
+		} else {
+			new Thread(() -> {
+				try {
+					String serverHash = getServerId((PacketHandlerLogin) (Object) this);
+					String encodedUser = URLEncoder.encode(packetLogin.username, "UTF-8");
+					String encodedServerId = URLEncoder.encode(serverHash, "UTF-8");
+
+					URL url = new URL("https://authserver.ely.by/session/legacy/hasJoined?user="
+						+ encodedUser + "&serverId=" + encodedServerId);
+					BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+					String response = reader.readLine();
+					reader.close();
+
+					if ("YES".equals(response)) {
+						setLoginPacket((PacketHandlerLogin) (Object) this, packetLogin);
+					} else {
+						this.kickUser("Failed to verify username!");
+					}
+				} catch (Exception e) {
+					LOGGER.error("Exception while trying to verify user '{}', kicking!", this.getUserAndIPString(), e);
+					this.kickUser("Failed to verify username! [internal error " + e + "]");
+				}
+			}).start();
+		}
+	}
 }
